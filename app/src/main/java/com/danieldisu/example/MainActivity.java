96d0ff607439749ae1d90e84d0583ff2c75af513
Daniel Diaz Suarez <ddiaz@autentia.com>
2014-05-29 10:03:46 +0200

    Added ButterKnife

--- app/src/main/java/com/danieldisu/example/MainActivity.java
+++ app/src/main/java/com/danieldisu/example/MainActivity.java
@@ -2,38 +2,39 @@ package com.danieldisu.example;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 
 public class MainActivity extends Activity {
+
+    @InjectView(R.id.firstEditText)
+    EditText firstEditText;
+
+    @InjectView(R.id.secondEditText)
+    EditText secondEditText;
+
+    @InjectView(R.id.thirdEditText)
+    EditText thirdEditText;
+
+    @InjectView(R.id.fourthEditText)
+    EditText fourthEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-
-        EditText firstEditText = (EditText) findViewById(R.id.firstEditText);
-        EditText secondEditText = (EditText) findViewById(R.id.secondEditText);
-        EditText thirdEditText = (EditText) findViewById(R.id.thirdEditText);
-        EditText fourthEditText = (EditText) findViewById(R.id.fourthEditText);
-
-        Button thatButton = (Button) findViewById(R.id.thatButton);
-
-        thatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                doSomethingWithTheButton();
-            }
-        });
+        ButterKnife.inject(this);
     }
 
-    private void doSomethingWithTheButton() {
-
+    @OnClick(R.id.thatButton)
+    public void doSomethingWithTheButton() {
+        Toast.makeText(this, firstEditText.getText().toString(), Toast.LENGTH_LONG).show();
     }
-
 
 }
